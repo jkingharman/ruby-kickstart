e299b6790098549b9dc3100f3f3e70e5e2f05717
Jasper King-Harman <j.kingharman@googlemail.com>
2017-05-15 14:30:36 +0100

    Adding encryptor logic/module

--- session6/challenge/sinatra_encrypter/encryptor.rb
+++ session6/challenge/sinatra_encrypter/encryptor.rb
@@ -1,12 +1,19 @@
 
-class Encryptor
+module Encryptor
 
-def initialize
+def create_ciphor
+  cipher_lower = Hash[('a'..'w').to_a.zip(('d'..'z').to_a)]
+  cipher_lower["x"] = "a"; cipher_lower["y"] = "b"; cipher_lower["z"] = "c"
+  cipher_upper = Hash[('A'..'W').to_a.zip(('D'..'Z').to_a)]
+  cipher_upper["X"] = "A"; cipher_lower["Y"] = "B"; cipher_lower["Z"] = "C"
+  combo_cipher = cipher_lower.merge(cipher_upper)
+  combo_cipher[" "] = " "
+  combo_cipher
 end
 
 def encrypt(message)
-  message
+  cipher = create_ciphor
+  message.chars.map {|letter| cipher[letter]}.join('')
 end
-
 
 end
--- session6/challenge/sinatra_encrypter/main.rb
+++ session6/challenge/sinatra_encrypter/main.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'shotgun'
 require_relative 'encryptor'
 
+include Encryptor
+
 get "/encrypt" do
 erb :new
 end
@@ -10,8 +12,7 @@ end
 # Functionining as your create controller.
 post "/encrypt" do
 
-encryptor = Encryptor.new
-encrypted_message = encryptor.encrypt(params[:message])
+encrypted_message = encrypt(params[:message])
 
 erb :index, locals: {encrypted_message: encrypted_message}
 
