c4d237ca92d61b54f868617cfa89c9c0e218b4d7
Jasper King-Harman <j.kingharman@googlemail.com>
2017-05-17 13:11:45 +0100

    Adding chap 1 challenges

--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,20 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+   return_odds ? odd_string = '' : even_string = ''
+  if return_odds
+     string.chars.each_with_index {|char, index| odd_string += char if index.odd?}
+     odd_string
+	 else
+		 string.chars.each_with_index {|char, index| even_string += char if index.even?}
+     even_string
+	 end
+
 end
--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,12 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  after_r = []
+  each_let = string.split(//)
+	each_let.each_with_index {|letter, i| after_r << each_let[i + 1] if letter =~ /r|R/}
+	after_r.join
 end
